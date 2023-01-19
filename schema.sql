931ed3f895646ac37ea0d6a028e178b86d4d7337
evdubs <1124030+evdubs@users.noreply.github.com>
2023-01-19 10:39:37 -1000

    add new price table to schema

--- schema.sql
+++ schema.sql
@@ -26,3 +26,16 @@ create table kraken.trade (
 
 create index on kraken.trade (timestamp, pair);
 
+create table kraken.usd_reference_price (
+  transaction_id text NOT null,
+  base_currency_pair text null,
+  base_currency_price numeric null,
+  base_currency_volume numeric null,
+  base_currency_timestamp timestamptz null,
+  counter_currency_pair text null,
+  counter_currency_price numeric null,
+  counter_currency_volume numeric null,
+  counter_currency_timestamp timestamptz null,
+  constraint usd_reference_price_pkey primary key (transaction_id),
+  constraint usd_reference_price_transaction_id_fkey foreign key (transaction_id) references kraken.trade(transaction_id)
+);
